d0bdffbd3ea91168a3d01bb6b53aa5f1f6aed235
reemshehayib <reemshehayib@gmail.com>
2026-01-08 22:25:49 -0500

    qualcomm edits-6

--- rover_receiver.ino
+++ rover_receiver.ino
@@ -1,12 +1,23 @@
+#include <zephyr/drivers/gpio.h> // Zephyr specific
+
+// This uses the board's actual built-in LED definition
+#define LED0_NODE DT_ALIAS(led0)
+
 void setup() {
   Serial.begin(115200); 
-  pinMode(13, OUTPUT);
+  // Use LED_BUILTIN - standard Arduino-to-Zephyr mapping
+  pinMode(LED_BUILTIN, OUTPUT); 
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    char val = Serial.read();
-    if (val == '1') digitalWrite(13, HIGH);
-    else if (val == '0') digitalWrite(13, LOW);
+    int incomingByte = Serial.read();
+    
+    // We check for both ASCII '1' and raw byte 1 just in case
+    if (incomingByte == '1' || incomingByte == 1) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    } else if (incomingByte == '0' || incomingByte == 0) {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
   }
 }
